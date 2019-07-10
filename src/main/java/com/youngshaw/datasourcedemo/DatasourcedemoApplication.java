ba7908633ab4ae2fa952c580b8065d6422f722fa
Administrator <youngshaw_vip@163.com>
2019-07-10 17:01:26 +0800

    [skip ci] create project

---
+++ src/main/java/com/youngshaw/datasourcedemo/DatasourcedemoApplication.java
@@ -0,0 +1,13 @@
+package com.youngshaw.datasourcedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourcedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourcedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youngshaw/datasourcedemo/DatasourcedemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.youngshaw.datasourcedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatasourcedemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
